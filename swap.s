dd59d2af5beba88c75bd13db37ba9b2bc12a2d10
Ayush Dixit <63745645+minecraftdixit@users.noreply.github.com>
2023-02-24 16:17:55 +0530

    Update swap.s

--- swap.s
+++ swap.s
@@ -1,16 +1,15 @@
 AREA SWAP,CODE,READONLY 
 ENTRY
-EXPORT main[D[D[D[D__main
-[A [A[A[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[AREA SWAP,CODE, READONLY
+EXPORT main
+  AREA SWAP,CODE, READONLY
                      ENTRY
                      EXPORT __main
-        __main
+         
                      LDR  R0 , =0xBABEFACE
                      LDR  R1  , =0xDEADBEEF
                      EOR R0, R0, R1
                     EOR R0, R0, R1
                      EOR R0, R0, R1
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[0[D[D[D[D[A[1[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[DSTOP
-                     [AB   STOP
+                     STOP
                      ALIGN
                       END
